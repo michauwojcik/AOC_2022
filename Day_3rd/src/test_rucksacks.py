291ad480460aa9f771be19dc2b200e346e774577
Michau Wojcik <michalstanislawwojcik@gmail.com>
2022-12-04 12:16:01 +0100

    tests for 'get_common_items' and 'get_rucksack_value' added

--- Day_3rd/src/test_rucksacks.py
+++ Day_3rd/src/test_rucksacks.py
@@ -5,16 +5,40 @@ import os
 sys.path.append(os.path.abspath(__file__ + "/../.."))
 from rucksacks import RucksackManager
 
-
 @pytest.mark.parametrize(
-    "test_input,expected", [
-        ("asdfhjka", 1),
-        ("obbi", 2),
-        ("aa", 1),
-        ("zioz", 26),
+    "test_input,expected", 
+    [
+        ("asdfhjka", {"a"}),
+        ("obbi",     {"b"}),
+        ("aa",       {"a"}),
+        ("zioz",     {"z"}),
+        ("CCABCC",   {"C"}),
+        ("CCAACC",   {"C", "A"})
     ]
 )
-def test_get_points_whole_game(test_input, expected):
+def test_get_common_items(test_input, expected):
+    rucksack = RucksackManager(test_input)
+    
+    assert rucksack.get_common_items() == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected", 
+    [
+        ("asdfhjka",  1),
+        ("obbi",      2),
+        ("aa",        1),
+        ("zioz",      26),
+        ("CCAACC",    56),
+        ("aAbCAa",    28)
+    ]
+)
+def test_get_rucksack_value(test_input, expected):
     rucksack = RucksackManager(test_input)
     
     assert rucksack.get_rucksack_value() == expected
+
+
+
+
+
